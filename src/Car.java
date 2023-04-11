7e8e865d9e408e154d5e6e3e500168d414a3b229
Михаил <m-toka@mail.ru>
2023-04-11 21:46:22 +0300

    Homework is done

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car extends Vehicle{
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine()  {
+        System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void check() {
+        super.check();
+        checkEngine();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("car1",4);
+        Car car2 = new Car("car2",4);
+
+        Truck truck1 = new Truck("truck1",6);
+        Truck truck2 = new Truck("truck2",8);
+
+        Bicycle bicycle1 = new Bicycle("bicycle1",2);
+        Bicycle bicycle2 = new Bicycle("bicycle2",2);
+
+        ServiceStation station = new ServiceStation();
+        station.service(car1);
+        System.out.println();
+        station.service(car2);
+        System.out.println();
+        station.service(truck1);
+        System.out.println();
+        station.service(truck2);
+        System.out.println();
+        station.service(bicycle1);
+        System.out.println();
+        station.service(bicycle2);
     }
 }
